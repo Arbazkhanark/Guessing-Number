0e5e8abd8ff450a58d5877196c93ea6df2834768
Arbaz Khan <arbaazkhanark23@gmail.com>
2022-07-13 18:23:48 +0530

    static page created

---
+++ guessingNumber.js
@@ -0,0 +1,10 @@
+'use strict';
+
+////console.log(document.querySelector('.message'));  //This will take the code and shown to console
+//eg: <p class="message">Start guessing...</p>
+
+
+console.log(document.querySelector('.message').textContent);
+
+////text.content takes only content from the code and shown to the console
+//eg.: Start guessing...
