426e8a602c84b79ddcbdab9ce37ce98bc6390926
Arbaz Khan <arbaazkhanark23@gmail.com>
2022-07-13 22:36:20 +0530

    guessing is implimented (high&low)

--- guessingNumber.js
+++ guessingNumber.js
@@ -6,33 +6,50 @@
 
 console.log(document.querySelector('.message').textContent);
 
-////text.content takes only content from the code and shown to the console
-//eg.: Start guessing...
+// ////text.content takes only content from the code and shown to the console
+// //eg.: Start guessing...
 
 
-document.querySelector('.message').textContent='Correct Number';
-//console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent='Correct Number';
+console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent=13;
-document.querySelector('.score').textContent=20
+document.querySelector('.number').textContent=11;
+document.querySelector('.score').textContent=20;
 
  
-document.querySelector('.guess').value=14;
+document.querySelector('.guess').value=5;
 console.log(document.querySelector('.guess').value);
-//Here we use value method because we wanted enter the number/input from the user.
-//That's why we use "value" method, we don't want textContent here.
+// //Here we use value method because we wanted enter the number/input from the user.
+// //That's why we use "value" method, we don't want textContent here.
 
 
-/*"Events"
+// /*"Events"
  
-An "Event" is something that happens on the page.
-eg: 'EventListener' , 'mouse click' , 'mouse moving' , 'key press' etc...
+// An "Event" is something that happens on the page.
+// eg: 'EventListener' , 'mouse click' , 'mouse moving' , 'key press' etc...
 
-Now we will use EventListener for button 
-:-'EventListener': we can wait for a certain event to happen,
-and then react to it.
-*/
+// Now we will use EventListener for button 
+// :-'EventListener': we can wait for a certain event to happen,
+// and then react to it.
+// */
+
+const system_number=Math.trunc (Math.random()*20)+1; 
+document.querySelector('.number').textContent=system_number;
+
+
+
 document.querySelector('.check').addEventListener('click', function(){
-    console.log(document.querySelector('.guess').value);
-    
-})
+const guess=Number(document.querySelector('.guess').value);
+
+// //console.log(guess, typeof guess);  //user input
+
+if(!guess){
+    document.querySelector('.message').textContent="OOPS!! No Number / OR  Invalid input";
+}else if(guess>system_number){
+    document.querySelector('.message').textContent="Guess is High";
+}else if(guess<system_number){
+    document.querySelector('.message').textContent="Guess is low";
+}else{
+    document.querySelector('.message').textContent="Congrats You Won!!!"
+}
+});
